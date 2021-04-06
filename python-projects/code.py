9fb2c2dc9d3994bb8f9fd199ea569a039faba88b
vimal2406 <61109665+vimal2406@users.noreply.github.com>
2021-04-06 23:26:35 +0530

    Code file created.

---
+++ python-projects/code.py
@@ -0,0 +1,73 @@
+# --------------
+# Code starts here
+
+# Create the lists 
+class_1 = ['Geoffrey Hinton','Andrew Ng','Sebastian Raschka','Yoshua Bengio']
+class_2 = ['Hilary Mason','Carla Gentry','Corinna Cortes']
+# Concatenate both the strings
+new_class = class_1 + class_2
+print(new_class)
+# Append the list
+new_class.append('Peter Warden')
+# Print updated list
+print(new_class)
+
+# Remove the element from the list
+new_class.remove('Carla Gentry')
+# Print the list
+print(new_class)
+courses = {'Math':65 , 'English':70 , 'History':80 , 'French':70 , 'Science':60} 
+values = courses.values()
+total = sum(values)
+print(total)
+percentage = total/500 * 100
+print(percentage)
+
+# Create the Dictionary
+mathematics = {'Geoffrey Hinton':78, 'Andrew Ng':95, 'Sebastian Raschka':65, 'Yoshua Bengio':50, 'Hilary Mason':70,  'Corinna Cortes':66, 'Peter Warden':75}
+
+max_marks_scored = max(mathematics,key = mathematics.get)
+topper = max_marks_scored
+print(max_marks_scored)
+
+# Slice the dict and stores  the all subjects marks in variable
+
+first_name = topper.split()[0]
+last_name = topper.split()[1]
+full_name = last_name + ' ' + first_name
+certificate_name = full_name.upper()
+print(certificate_name)
+
+
+# Store the all the subject in one variable `Total`
+
+# Print the total
+
+# Insert percentage formula
+
+# Print the percentage
+
+
+
+
+# Create the Dictionary
+ 
+
+
+
+# Given string
+
+
+# Create variable first_name 
+
+# Create variable Last_name and store last two element in the list
+
+# Concatenate the string
+
+# print the full_name
+
+# print the name in upper case
+
+# Code ends here
+
+
